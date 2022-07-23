dfea7f57e3d2848a82a87f9447191f6b814e1721
Kelly Zhou <kellyzhou@Rongmaos-MacBook-Pro-2.local>
2022-07-23 00:53:36 -0500

    fixed event tile width

--- components/about.js
+++ components/about.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 function AboutItem(props) {
     return (
-        <div className='px-6 space-y-4 p-10 bg-c20 mt-8 rounded-3xl max-w-md mx-auto'>
+        <div className='px-6 space-y-4 p-10 bg-c20 mt-8 rounded-3xl max-w-md flex-1'>
             <h4 className='text-center font-bold text-2xl text-white'>{props.title}</h4>
             <div className='flex flex-col justify-around h-[425px]'>
                 <div className='m-auto pb-10 lg:max-w-xs lg:pb-6 lg:px-0'>
