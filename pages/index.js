55ec98916b77a7b848feef2919caf9ed5c48ddeb
Kelly Zhou <kellyzhou@Rongmaos-MacBook-Pro-2.local>
2022-07-24 22:07:29 -0500

    adjusted for scaling pt.2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import NavbarPage from '../components/navbar'
 import TrackInfo from '../components/tracks'
 import ChallengeInfo from '../components/challenges'
@@ -87,7 +88,8 @@ export default function Home() {
         <meta property="og:title" content="hackTAMS 2023"/>
         <meta property="og:description" content="hackTAMS is an annual hackathon organized by students at the Texas Academy of Mathematics and Science for students in the Dallas-Fort Worth area."/>
         <meta property="og:image" content="https://2023.hacktams.org/titlePage.png"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/> 
+        {/*<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>*/}
+        <meta name="viewport" content="width=device-width, initial-scale='(1/window.devicePixelRatio)', viewport-fit=cover"/>
         <meta name="theme-color" content="#7f8aad"/>
       </Head>
 
@@ -309,6 +311,7 @@ export default function Home() {
                 </div>
             </div>
         </footer>
+
     </div>
   )
 }
