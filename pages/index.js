65d00626b2bbd4251cee8d61a18997db3df69abf
Kelly Zhou <kellyzhou@Rongmaos-MacBook-Pro-2.local>
2022-07-24 22:22:05 -0500

    took out window scaling, fixed drop-down menu

--- pages/index.js
+++ pages/index.js
@@ -88,8 +88,7 @@ export default function Home() {
         <meta property="og:title" content="hackTAMS 2023"/>
         <meta property="og:description" content="hackTAMS is an annual hackathon organized by students at the Texas Academy of Mathematics and Science for students in the Dallas-Fort Worth area."/>
         <meta property="og:image" content="https://2023.hacktams.org/titlePage.png"/>
-        {/*<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>*/}
-        <meta name="viewport" content="width=device-width, initial-scale='(1/window.devicePixelRatio)', viewport-fit=cover"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
         <meta name="theme-color" content="#7f8aad"/>
       </Head>
 
@@ -97,7 +96,7 @@ export default function Home() {
         <NavbarPage/>
         <div className='flex flex-col justify-around max-h-fit'>
           {/*text div*/}
-          <div className='mt-16 sm:mt-10 md:mt-0 space-y-12 relative'> 
+          <div className='mt-16 sm:mt-10 md:mt-0 space-y-12'> 
             <div>
               <h1 className='font-bold text-center text-white text-6xl sm:text-7xl md:text-8xl lg:text-9xl'>hackTAMS</h1>
               <h1 className='font-bold text-center text-white text-6xl sm:text-7xl md:text-8xl lg:text-9xl'>2023</h1>
